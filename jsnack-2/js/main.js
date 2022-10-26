b10e06eab6e9ce4b6553ac37a2acae0b899b62cd
Vale-riad <valentinariad91@gmail.com>
2022-10-26 12:11:00 +0200

    Corr eserzio 2

--- jsnack-2/js/main.js
+++ jsnack-2/js/main.js
@@ -1,7 +1,9 @@
-let numbers = Number(prompt("inserisci 10 numeri"));
+
 
 
 for (let index = 0; index < 11; index++) {
-    console.log(index);
+    let numbers = Number(prompt("inserisci 10 numeri"));
+
+    console.log(index,numbers);
     
 }
